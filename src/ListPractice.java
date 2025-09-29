f2e6cd8135ec22832c9d801a9d1be9d7a1333912
Jessica H <jessicahebert75@gmail.com>
2025-09-29 16:08:10 -0700

    Demonstrated ArrayList operations (create, add, access, update, print)

--- src/ListPractice.java
+++ src/ListPractice.java
@@ -1,16 +1,23 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListPractice {
 
 
-  public static void main(String[] args) {
+  public static void main(String[] args) {  
     // Create an empty ArrayList of Strings and assign it to a variable of type List
-
+    List <String> newList = new ArrayList<>(); 
     // Add 3 elements to the list (OK to do one-by-one)
-
+    newList.add("Jessica");
+    newList.add("Awesum");
+    newList.add("Gizmo");
     // Print the element at index 1
-
+    System.out.println(newList.get(1));
     // Replace the element at index 1 with a new value
     // (Do not insert a new value. The length of the list should not change)
-
+    newList.set(1, "DeAndre");
+    System.out.println(newList);
     // Insert a new element at index 0 (the length of the list will change)
 
     // Check whether the list contains a certain string
