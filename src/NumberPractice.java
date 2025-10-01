477b58eb3b278e31977e643f11b98b6142301880
Lilian <210286008+liliann19@users.noreply.github.com>
2025-09-30 23:12:00 -0700

    Determine whether the num is even or odd

--- src/NumberPractice.java
+++ src/NumberPractice.java
@@ -15,6 +15,11 @@ public class NumberPractice {
     // (A number is even if it has a remainder of zero when divided by 2)
     // Use an if-else to print "Even" if the number is even and "Odd"
     // if the number is odd.
+    if (positiveNum % 2 == 0){
+      System.out.println("Even");
+    } else {
+      System.out.println("Odd");
+    }
 
     // Divide the number by another number using integer division
 
