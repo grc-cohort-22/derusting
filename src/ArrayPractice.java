701eb23f472132907ac4b379000cad950d2567f6
Lilian <210286008+liliann19@users.noreply.github.com>
2025-10-01 11:47:41 -0700

    Created array of Strings, update toRefresh

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -1,6 +1,8 @@
+
 public class ArrayPractice {
   public static void main(String[] args) {
     // Create an array of Strings of size 4
+    String[] array = new String[4];
 
     // Set the value of the array at each index to be a different String
     // It's OK to do this one-by-one
--- toRefresh.md
+++ toRefresh.md
@@ -8,3 +8,4 @@ As you work through this exercise, write down anything that you needed to look u
 - .set() to replace an element at index
 - .size() to get number of elements in list
 - Collections.sort()
+- Arrays vs Lists
