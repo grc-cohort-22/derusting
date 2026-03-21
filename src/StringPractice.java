5913ef717d960a866221307b7612fe4c75c4ec39
Jaron <jayisaac6706@gmail.com>
2026-03-20 23:00:57 -0700

    Importing ArrayList

--- src/StringPractice.java
+++ src/StringPractice.java
@@ -1,4 +1,7 @@
-public class StringPractice {
+import java.util.ArrayList;
+
+public class StringPractice {  
+  
   public static void main(String[] args) {
     // Create a string with at least 5 characters and assign it to a variable
     String fiveLetterString = "Hello";
