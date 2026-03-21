503cbb6e6b8997975fb163c7a9dd342ee18afb18
KJ <kjmiclea@gmail.com>
2026-03-20 17:21:55 -0700

    Java Derusting complete!

--- src/Person.java
+++ src/Person.java
@@ -45,16 +45,17 @@ public int birthYear(int currentYear) {
     // assign it to a different variable
     Person ron = new Person("ronald", 87);
     // Print the first person
-    
+    System.out.println(larry.toString());
     // Print the second person
-
+    System.out.println(ron.toString());
     // Get the name of the first person and store it in a local variable
-
+    String larry_name = larry.name;
     // Using the birthYear method, get the birth year of the first person
     // and store it in a local variable. Input the actual current year (e.g. 2025)
     // as the argument.
-
+    int birthyear = larry.birthYear(2026);
     // In a separate statement, print the local variable holding the birth year.
+    System.out.println(birthyear);
 
     /**
      * Terminology!
